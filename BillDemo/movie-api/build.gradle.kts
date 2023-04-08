a7b2ea7b70ba6ed13105a78c2b368daf739971ba
ShrikantND <130126579+ShrikantND@users.noreply.github.com>
2023-04-08 09:33:16 -0700

    Update build.gradle.kts

--- BillDemo/movie-api/build.gradle.kts
+++ BillDemo/movie-api/build.gradle.kts
@@ -7,12 +7,10 @@ tasks.getByName("jar") {
 }
 
 dependencies {
-	//implementation(":movie-entity")
 	api(project(":movie-entity"))
 	api(project(":movie-security"))
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	testImplementation("org.springframework.security:spring-security-test")
-	//runtimeOnly("com.h2database:h2")
 }
