25aeebc6d1d70e9cde2d433553c01b02021991fe
ShrikantND <130126579+ShrikantND@users.noreply.github.com>
2023-04-08 09:33:35 -0700

    Update build.gradle.kts

--- BillDemo/movie-entity/build.gradle.kts
+++ BillDemo/movie-entity/build.gradle.kts
@@ -1,14 +1,4 @@
 
-//plugins {
-//	kotlin("plugin.allopen") version "1.8.0"
-//
-//}
-//
-//allOpen {
-//	annotation("jakarta.persistence.Entity")
-//	annotation("jakarta.persistence.Embeddable")
-//	annotation("jakarta.persistence.MappedSuperclass")
-//}
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
